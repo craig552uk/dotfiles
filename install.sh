cd0b236bbc4db2bfed36ab5264b0c8bdfaedc116
Craig Russell <craig@craig-russell.co.uk>
2014-06-30 09:57:50 +0100

    Symlinks to application configs

--- install.sh
+++ install.sh
@@ -9,10 +9,14 @@ ln --force --symbolic --verbose $SCRIPT_DIR/bashrc $HOME/.bashrc
 ln --force --symbolic --verbose $SCRIPT_DIR/conkyrc $HOME/.conkyrc
 ln --force --symbolic --verbose $SCRIPT_DIR/vimrc $HOME/.vimrc
 ln --force --symbolic --verbose $SCRIPT_DIR/irbrc $HOME/.irbrc
-ln --force --symbolic --verbose $SCRIPT_DIR/terminator_config $HOME/.config/terminator/config
 ln --force --symbolic --verbose $SCRIPT_DIR/bashrc $HOME/.bashrc
+
+# Creating symlinks to application configs
+ln --force --symbolic --verbose $SCRIPT_DIR/terminator_config $HOME/.config/terminator/config
+ln --force --symbolic --verbose $SCRIPT_DIR/inkscape_default.svg $HOME/.config/inkscape/templates/default.svg
 
 # Creating symlinks to directories
 ln --force --symbolic --verbose --no-target-directory $SCRIPT_DIR/bin $HOME/bin
 ln --force --symbolic --verbose --no-target-directory $SCRIPT_DIR/fonts $HOME/.fonts
 ln --force --symbolic --verbose --no-target-directory $SCRIPT_DIR/icons $HOME/.icons
+
