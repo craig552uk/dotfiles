5dee047745829ed5f7c21c643ee94ae176951586
Craig Russell <craig@craig-russell.co.uk>
2014-06-30 14:39:28 +0100

    Fixed yui compressor setup bug. Install packages seperately to avoid fail blocking

--- install.sh
+++ install.sh
@@ -10,9 +10,17 @@ sudo add-apt-repository --yes ppa:gnome-terminator
 sudo apt-get update
 
 # Install packages
-sudo apt-get install --yes curl git-core vim sublime-text terminator conky \
-python-dev python-pip python-virtualenv \
-inkscape gimp firefox openjdk-6
+sudo apt-get install --yes curl
+sudo apt-get install --yes git-core
+sudo apt-get install --yes vim
+sudo apt-get install --yes sublime-text
+sudo apt-get install --yes terminator
+sudo apt-get install --yes conky
+sudo apt-get install --yes python-dev python-pip python-virtualenv
+sudo apt-get install --yes inkscape
+sudo apt-get install --yes gimp
+sudo apt-get install --yes firefox
+sudo apt-get install --yes openjdk-6
 
 echo "Creating Symlinks to configs ..."
 
@@ -34,8 +42,8 @@ ln --force --symbolic --verbose --no-target-directory $SCRIPT_DIR/icons $HOME/.i
 
 # Download YUICompressor
 mkdir $HOME/.yuicompressor
-$YUI_IN="https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar"
-$YUI_OUT="$HOME/.yuicompressor/yuicompressor-latest.jar"
+YUI_IN="https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar"
+YUI_OUT="$HOME/.yuicompressor/yuicompressor-latest.jar"
 wget --output-document $YUI_OUT $YUI_IN
 
 # Non PPA packages
