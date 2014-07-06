ceac6bad5031453ffd7b5c6a187d14af455beef0
Craig Russell <craig@craig-russell.co.uk>
2014-07-06 10:48:35 +0100

    Added i3 window manager and i3 status configs

--- install.sh
+++ install.sh
@@ -37,12 +37,15 @@ ln --force --symbolic --verbose $SCRIPT_DIR/conkyrc $HOME/.conkyrc
 ln --force --symbolic --verbose $SCRIPT_DIR/vimrc $HOME/.vimrc
 ln --force --symbolic --verbose $SCRIPT_DIR/irbrc $HOME/.irbrc
 ln --force --symbolic --verbose $SCRIPT_DIR/bashrc $HOME/.bashrc
+ln --force --symbolic --verbose $SCRIPT_DIR/i3status.conf $HOME/.i3status.conf
 
 # Creating symlinks to application configs
 mkdir --parents $HOME/.config/terminator
 ln --force --symbolic --verbose $SCRIPT_DIR/terminator_config $HOME/.config/terminator/config
 mkdir --parents $HOME/.config/inkscape/templates
 ln --force --symbolic --verbose $SCRIPT_DIR/inkscape_default.svg $HOME/.config/inkscape/templates/default.svg
+mkdir --parents $HOME/.i3
+ln --force --symbolic --verbose $SCRIPT_DIR/i3wm.conf $HOME/.i3/config
 
 # Creating symlinks to directories
 ln --force --symbolic --verbose --no-target-directory $SCRIPT_DIR/bin $HOME/bin
