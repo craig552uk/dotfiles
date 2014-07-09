d0b7fda83abf5ec2c466fb609d46236e22daedca
Craig Russell <craig@craig-russell.co.uk>
2014-07-09 16:22:07 +0100

    Removed redundant line

--- install-dropbox.sh
+++ install-dropbox.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 URL="https://www.dropbox.com/download?plat=lnx.x86_64"
-TMP="/tmp/google-chrome-stable_current_amd64.$$.deb"
 
 # Download and extract
 cd ~ && wget -O - $URL | tar xzf -
