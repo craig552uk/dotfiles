917b640a7817e8078b660a667e6f85749504f18c
Craig Russell <craig@craig-russell.co.uk>
2014-08-04 22:01:50 +0100

    Updated Firefox

--- install-firefox.sh
+++ install-firefox.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-URL="https://download.mozilla.org/?product=firefox-30.0-SSL&os=linux64&lang=en-GB"
-TMP="/tmp/firefox-30.0.$$.tar.bz2"
+# URL="https://download.mozilla.org/?product=firefox-30.0-SSL&os=linux64&lang=en-GB"
+URL="https://download.mozilla.org/?product=firefox-31.0-SSL&os=linux64&lang=en-GB"
+TMP="/tmp/firefox.$$.tar.bz2"
 
 # Uninstall iceweasel on debian
 sudo apt-get remove --yes iceweasel
@@ -13,7 +14,7 @@ wget --output-document $TMP $URL
 cd /opt && sudo tar -xvf $TMP
 
 # Create application link
-sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+sudo ln -sf /opt/firefox/firefox /usr/bin/firefox
 
 # Clean up
 rm -f $TMP
